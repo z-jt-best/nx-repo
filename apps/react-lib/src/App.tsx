e7b3c90215584a20b73d30b310976c84d3da5d12
zhangJt <907934985@qq.com>
2024-07-07 15:50:09 +0800

    init: add react-lib, vue-lib and shared-js libs

---
+++ apps/react-lib/src/App.tsx
@@ -0,0 +1,12 @@
+import { sayHi } from 'shared-js'
+
+function App() {
+  return (
+    <div>
+      <p>123</p>
+      <button onClick={() => sayHi}>sayHi</button>
+    </div>
+  )
+}
+
+export default App
---
+++ apps/vue-lib/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { sayHi } from 'shared-js'
+</script>
+
+<template>
+  <div>
+    <span> 123</span>
+    <button @click="sayHi">sayHi</button>
+  </div>
+</template>
